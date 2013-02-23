1a8e18a59a981c7453d9dc68670f9abb29ce3ba3
XlegitXcrazymanX <XlegitXcrazymanX@gmail.com>
2013-02-22 20:35:59 -0600

    Adding config.yml and new source

--- src/me/iFightBack/WhoisOnline/Core.java
+++ src/me/iFightBack/WhoisOnline/Core.java
@@ -8,24 +8,47 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Core extends JavaPlugin {
+	/*
+	Hi bukkit dev staff.
+	I know you are working very hard to review plugins for us!
+	I love you all very much <3
+	-ifightback\\Xlegit
+	*/
 	public void onEnable() {
-		getConfig();
+	getServer().getLogger().info("[Who is staff] has been enabled!");
+		this.getConfig();
+		this.getConfig().options().copyDefaults(true);
+	    this.saveDefaultConfig();
+
 	}
 	public void onDisable() {
-		
+		getServer().getLogger().info("[Who is staff] has been disabled!");
 	}
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		String ServerName = ChatColor.BLUE+"Smoothcraft";
+		
+		String ServerName = ChatColor.GOLD+"";
 		Player player = (Player) sender;
 		if(cmd.getName().equalsIgnoreCase("staff")) {
-		player.sendMessage(ChatColor.GOLD+"==="+this.getConfig().getString(ServerName)+"===");
+		player.sendMessage(ChatColor.GOLD+"==="+this.getConfig().getString("ServerName")+"===");
 		for (Player p : Bukkit.getOnlinePlayers()) {
+		//developer
 		if(p.hasPermission("whoisstaff.developer")){
-		player.sendMessage(ChatColor.GREEN +p.getName()+"   Developer");
-			
-				}
-			}
+			player.sendMessage(ChatColor.GREEN +p.getName()+"   Developer");
+		//admin	
+		if(p.hasPermission("whoisstaff.admin")){
+			player.sendMessage(ChatColor.RED +p.getName()+"   Admin");
+		//moderator
+		if(p.hasPermission("whoisstaff.moderator")) {
+			player.sendMessage(ChatColor.DARK_GREEN +p.getName()+"   Moderator");
+		//owner
+		if(p.hasPermission("whoisstaff.owner")) {
+			player.sendMessage(ChatColor.BLUE +p.getName()+"   Owner");
 		}
-	return false;
+		}
+		}
+		}
+		}
+		}
+	return true;
 	}
 }
